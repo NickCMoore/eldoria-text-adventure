57929b63d3a6ca23b36933170f9fddc5396c5cce
NickCMoore <moorenicholasc@gmail.com>
2023-12-13 14:11:38 +0000

    Add difficulty function to intro

--- run.py
+++ run.py
@@ -22,11 +22,29 @@ class Wizard(Characters):
     def __init__(self, name, description):
         super().__init__(name="Wizard", description="Eldron the Wise is a venerable wizard with a long white beard and a staff adorned with mystical runes.")
 
+def choose_difficulty():
+    """
+    Function for selecting difficulty level
+    """
+    print("Choose your difficulty:")
+    print("1. Easy")
+    print("2. Medium")
+    print("3. Hard")
+
+    while True:
+        choice = input("Which level do you choose?\n")
+        if choice in ['1', '2', '3']:
+            return int(choice)
+        else:
+            print("Invalid choice. Please enter a valid number")
+
 # Introduction
 def game_intro():
     print("Welcome to the Eldoria Text Adventure!\n")
-    player_name = input("Enter your name: ")
+    player_name = input("Enter your name: \n")
     difficulty = choose_difficulty()
     player = Player(player_name, difficulty)
+
+
 
 game_intro()
