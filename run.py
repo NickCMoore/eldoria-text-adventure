e9c1a0e4f9e7865bb7dbdfc5fa2a454b4bf0c637
NickCMoore <>
2023-12-14 15:23:23 +0000

    Link up APIs in run.py and confirm access to Google Sheet

--- run.py
+++ run.py
@@ -1,3 +1,23 @@
+import gspread
+from google.oauth2.service_account import Credentials
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('eldoria-text-adventure')
+
+leaderboard = SHEET. worksheet('leaderboard')
+
+data = leaderboard.get_all_values()
+
+print(data)
+
 # Game items
 
 class Item():
